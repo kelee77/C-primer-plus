94bf08c0156ac576e4a031a4e168449dfcdd272a
sunhongyin <sunhongyin@sunhongyindemacbook-pro.local>
2016-03-06 16:32:48 +0800

    init

--- getinfo/main.cpp
+++ getinfo/main.cpp
@@ -8,8 +8,18 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main(int argc, const char * argv[])
+{
+    using namespace std;
+    
+    int carrots;
+    
+    cout << "How many carrots do you have?" << endl;
+    cin >> carrots;
+    cout << "Here are tow more. ";
+    
+    carrots = carrots + 2;
+    cout << "Now you have " << carrots << " carrots. " << endl;
+    
     return 0;
 }
